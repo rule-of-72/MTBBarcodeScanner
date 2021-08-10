68f3694b23c4751afb80922e315d60e14babe739
Andrew Reeman <areeman@controlpoint.co.uk>
2021-08-10 10:41:28 +0100

    Trying to expose objective-c module: https://forums.swift.org/t/can-i-use-swift-package-manager-in-obj-c-code/35463/9

--- Package.swift
+++ Package.swift
@@ -21,7 +21,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MTBBarcodeScanner",
-            dependencies: []
+            dependencies: [],
+            path: "Sources",
+            publicHeadersPath: "includes"
             )
     ]
 )
