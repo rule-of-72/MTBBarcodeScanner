7ca830f4f20a91c9384a84600f06b2be3915109f
Andrew Reeman <areeman@controlpoint.co.uk>
2021-08-10 10:16:27 +0100

    Dependencies were picking up package but not module. maybe moving everything into sources helps?

--- Package.swift
+++ Package.swift
@@ -21,8 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MTBBarcodeScanner",
-            dependencies: [],
-            path: "Classes/ios/Scanners"
+            dependencies: []
             )
     ]
 )
